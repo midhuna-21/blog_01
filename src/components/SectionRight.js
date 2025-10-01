96815061e1c39cf51b10cdc186c60f00ec4f29d5
midhuna-21 <krishnamidhuna850@gmail.com>
2025-10-01 16:12:15 +0530

    alignment issues completed01

--- src/components/SectionRight.js
+++ src/components/SectionRight.js
@@ -3,7 +3,7 @@ import SocialLinks from "./SocialLinks";
 
 export default function SectionRight({ sections, activeSlug }) {
     return (
-        <div className="md:w-1/2 flex flex-col md:space-y-9 space-y-7 flex-1 md:py-28">
+        <div className="md:w-1/2 flex flex-col md:space-y-9 space-y-7 flex-1 md:py-33">
             {sections.map((section, idx) => (
                 <Link
                     key={idx}
